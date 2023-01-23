d7e92da1365a27a5ca9dfa2df787712f3fff65ce
JuanPabloChiconi <121985467+JuanPabloChiconi@users.noreply.github.com>
2023-01-23 15:16:31 -0300

    Add files via upload

---
+++ CuartaEntrega2/AppCoder/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+"""
+class CursoFormulario(forms.Form):
+
+    #Especificar los campos
+    curso = forms.CharField()
+    camada = forms.IntegerField()
+
+
+class ProfesorFormulario(forms.Form):   
+    nombre= forms.CharField(max_length=30)
+    apellido= forms.CharField(max_length=30)
+    email= forms.EmailField()
+    profesion= forms.CharField(max_length=30)
+    
+"""
+
+
+class SensoresFormulario(forms.Form):   
+    identificacion= forms.CharField(max_length=30)
+    modelo= forms.CharField(max_length=30)
+    valor= forms.CharField(max_length=30)
+
+class DatosPersonalesFormulario(forms.Form):   
+    direccion= forms.CharField(max_length=30)
+    telefono= forms.CharField(max_length=30)
+    email= forms.EmailField()
+
+class UsuarioFormulario(forms.Form):   
+    nombre= forms.CharField(max_length=30)
+    apellido= forms.CharField(max_length=30)
+    dni= forms.CharField(max_length=30)
+    
+
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        # Saca los mensajes de ayuda
+        help_texts = {k:"" for k in fields}
